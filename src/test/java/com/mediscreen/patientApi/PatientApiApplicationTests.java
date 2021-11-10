6c9d72595ff86c9b9a93f9bf5ef564d6fc0a4cb7
Cedric Ferron <cedric.ferron@axa.fr>
2021-11-10 16:54:29 +0100

    initial config commit

---
+++ src/test/java/com/mediscreen/patientApi/PatientApiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mediscreen.patientApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PatientApiApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
