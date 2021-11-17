ccca7652db22ad588689058787a399420dceed17
xhxcx <xxhxcxx.sn@gmail.com>
2021-11-17 10:02:54 +0100

    fix acces to swagger-ui and add documentation on endpoints + add properties to change serverport

--- src/main/java/com/mediscreen/patientApi/config/SwaggerConfig.java
+++ src/main/java/com/mediscreen/patientApi/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())//basePackage("com.mediscreen.patientApi.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.mediscreen.patientApi.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
--- src/main/java/com/mediscreen/patientApi/controller/PatientController.java
+++ src/main/java/com/mediscreen/patientApi/controller/PatientController.java
@@ -3,6 +3,7 @@ package com.mediscreen.patientApi.controller;
 import com.mediscreen.patientApi.model.PatientDto;
 import com.mediscreen.patientApi.service.PatientService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @Controller
-@Api(value = "API to manage patient personnal informations")
+@Api("API to manage patient personnal informations")
 public class PatientController {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(PatientController.class);
@@ -23,12 +24,14 @@ public class PatientController {
     @Autowired
     private PatientService patientService;
 
+    @ApiOperation(value = "get all patient as a list of patient")
     @GetMapping("/")
     public ResponseEntity<List<PatientDto>> getAll(){
         LOGGER.info("GET /getAll");
         return new ResponseEntity<>(patientService.getAllPatients(), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "find a patient with its id")
     @GetMapping(value = "/{id}")
     public ResponseEntity<PatientDto> findPatient(@PathVariable("id") int id) {
         LOGGER.info("GET /findPatient patientId = " + id);
@@ -36,12 +39,14 @@ public class PatientController {
         return patientDto != null ? new ResponseEntity<>(patientDto, HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    @ApiOperation(value = "update a patient")
     @PutMapping(value = "/{id}")
     public ResponseEntity<PatientDto> updatePatient(@Valid @RequestBody PatientDto patientDto){
         LOGGER.info("PUT /updatePatient patientId = " + patientDto.getId());
         return new ResponseEntity<>(patientService.savePatient(patientDto), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "create a new patient")
     @PostMapping(value = "/")
     public ResponseEntity<PatientDto> createPatient(@Valid @RequestBody PatientDto patientDto){
         LOGGER.info("POST /createPatient patient full name = " + patientDto.getFirstName() + " - " + patientDto.getLastName());
